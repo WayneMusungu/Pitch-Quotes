8f582ac5478744dba3967a2e4e81f624d01518aa
WayneMusungu <waynemuyera17@gmail.com>
2022-05-07 20:21:17 +0300

    Add index.html and extend it to home.html

--- pitch.py
+++ pitch.py
@@ -1,7 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Wayne Ortiz',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 12, 1924'
+    },
+      {
+        'author': 'Wo Shifu',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 12, 1924'
+    },
+    
+]
+
+
+
 @app.route("/")
-def hello():
-    return "Hello Wayne"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
