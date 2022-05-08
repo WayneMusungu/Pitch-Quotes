5c440a83e313f57d1e955dcbb22b33ce2deb04d8
WayneMusungu <waynemuyera17@gmail.com>
2022-05-08 07:23:50 +0300

    Add error conditions to the forms input field

--- forms.py
+++ forms.py
@@ -4,13 +4,13 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=25)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
     
     email = StringField('Email', validators=[DataRequired(), Email()])
     
     password = PasswordField('Password', validators=[DataRequired()])
 
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password dont match')])
     
     submit = SubmitField('Sign Up')
     
