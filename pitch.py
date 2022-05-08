a97e2bf3d1cdbea6c5f56f5212880bd5c8754e34
WayneMusungu <waynemuyera17@gmail.com>
2022-05-08 05:31:17 +0300

    Add new branch and make changes

--- pitch.py
+++ pitch.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
+
 
 posts = [
     {
@@ -19,24 +20,14 @@ posts = [
 ]
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-
-
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
