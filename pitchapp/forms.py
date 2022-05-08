8f203153d5aa04f56eb81b5b3ac099e59a3a52ea
WayneMusungu <waynemuyera17@gmail.com>
2022-05-08 21:16:09 +0300

    Create a route for new post

--- pitchapp/forms.py
+++ pitchapp/forms.py
@@ -1,8 +1,9 @@
 import email
+from turtle import title
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from pitchapp.models import User
 
@@ -64,6 +65,13 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email already exists. Please choose a different one ')
+        
+        
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    
     
     
 
--- pitchapp/routes.py
+++ pitchapp/routes.py
@@ -4,7 +4,7 @@ import secrets
 from PIL import Image
 from flask import render_template,url_for,flash,redirect,request
 from pitchapp import app, db, bcrypt
-from pitchapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from pitchapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from pitchapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -118,9 +118,14 @@ def account():
     return render_template('account.html', title='Account', image_file=image_file, form=form)  
 
 
-@app.route('/post/new')
+@app.route('/post/new', methods=['GET','POST'])
 @login_required
 def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been uploaded', 'alert alert-success' )
+        return redirect(url_for('home'))
+        
     return render_template('create_post.html', title='New Post')  
 
     
