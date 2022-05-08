5b613483ff9994eecdbfeb0d7a5bb945a21210a1
WayneMusungu <waynemuyera17@gmail.com>
2022-05-08 20:43:13 +0300

    Enable user to update their profile picture

--- pitchapp/forms.py
+++ pitchapp/forms.py
@@ -1,5 +1,6 @@
 import email
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -44,7 +45,7 @@ class UpdateAccountForm(FlaskForm):
     
     email = StringField('Email', validators=[DataRequired(), Email()])
     
-    # picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     
     submit = SubmitField('Update')
     
@@ -54,7 +55,7 @@ class UpdateAccountForm(FlaskForm):
         
             user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one ')
+            raise ValidationError('That username already exists. Please choose a different one ')
         
     def validate_email(self, email):
         
@@ -62,7 +63,7 @@ class UpdateAccountForm(FlaskForm):
         
             user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one ')
+            raise ValidationError('That email already exists. Please choose a different one ')
     
     
 
